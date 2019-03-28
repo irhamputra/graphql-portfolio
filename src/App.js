7a8eafb14d86540c705de17db35857c2e7132113
Muhamad Irham Prasetyo <muhamad.prasetyo@enerxess.com>
2019-03-28 22:28:38 +0100

    async await fetch

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import fetch from 'isomorphic-fetch';
 
 import 'graphiql-material-theme';
 import './index.css'
-import defautlQuery from "./defaultQuery";
+import defaultQuery from "./defaultQuery";
 
 class App extends Component {
 	state = {
 		url: 'http://localhost:4000/',
 	};
 	
-	graphQLFetcher = body =>
-		fetch(this.state.url, {
+	graphQLFetcher = async body => {
+		const option = {
 			method: 'post',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(body)
-		}).then(res => res.json());
+		};
+		
+		const res = await fetch(this.state.url, option);
+		
+		return await res.json()
+	};
 	
 	render() {
 		return (
 			<div style={{'height': '100%'}}>
-				<GraphiQL query={defautlQuery} editorTheme="material" fetcher={this.graphQLFetcher}>
+				<GraphiQL query={defaultQuery} editorTheme="material" fetcher={this.graphQLFetcher}>
 					<GraphiQL.Logo>
 						Muhamad Irham Prasetyo
 					</GraphiQL.Logo>
