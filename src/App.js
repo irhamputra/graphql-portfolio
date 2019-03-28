2683e973c98dbd8fe8aa65c3dda092ff33bb761d
Muhamad Irham Prasetyo <muhamad.prasetyo@enerxess.com>
2019-03-28 13:41:21 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import 'graphiql-material-theme';
+import './index.css'
+import defautlQuery from "./defaultQuery";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		url: 'https://api.graph.cool/simple/v1/cixos23120m0n0173veiiwrjr',
+		token: ''
+	};
+	
+	graphQLFetcher = body =>
+		fetch(this.state.url, {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${this.state.token}`
+			},
+			body: JSON.stringify(body)
+		}).then(res => res.json());
+	
+	render() {
+		return (
+			<div style={{'height': '100%'}}>
+				<GraphiQL query={defautlQuery} editorTheme="material" fetcher={this.graphQLFetcher}>
+					<GraphiQL.Logo>
+						My CV
+					</GraphiQL.Logo>
+				</GraphiQL>
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/defaultQuery.js
@@ -0,0 +1,18 @@
+const defautlQuery = `
+	# Hi!👋🏼 My name is Muhamad Irham Prasetyo
+	# I am a Front-end Developer from Indonesia 🇮🇩 based in Leipzig 🇩🇪
+	# I made this GraphQL IDE as my CV because everyone loves GraphQL
+	# Also GraphQL is more way easier to make REST
+	# You can fetch, which data do you want to know about me
+	# Check out the docs which information do you want to know
+	# Here is the sample data and click play
+	
+	{
+		allActors {
+			id
+			name
+		}
+	}
+`
+
+export default defautlQuery;
